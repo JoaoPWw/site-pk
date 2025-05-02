05ff0370b8401d406f37f4d39af80508ee61d4d2
JoaoPWw <wandall.joaopedro@gmail.com>
2025-05-02 00:29:31 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Configurações do Next.js podem ser adicionadas aqui se necessário no futuro
-};
-
-module.exports = nextConfig;
-
