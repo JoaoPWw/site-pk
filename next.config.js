49b6ed4f559f85e1a31e30a80db659bebc336694
JoaoPWw <wandall.joaopedro@gmail.com>
2025-05-02 00:12:25 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configurações do Next.js podem ser adicionadas aqui se necessário no futuro
+};
+
+module.exports = nextConfig;
+
